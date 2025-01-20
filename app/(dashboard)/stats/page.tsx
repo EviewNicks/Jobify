f5c41f4ce62f45aad454ec7d34be7a7d7476d116
Ardiansyah <eviewnicks@gmail.com>
2025-01-20 09:54:11 +0800

    Dashboard Layout, Sidebar and Navbar

--- app/(dashboard)/stats/page.tsx
+++ app/(dashboard)/stats/page.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-
 const StatsPage = () => {
-  return <h1 className="text-4xl">Jobs Page</h1>;
+  return <h1 className="text-4xl">Stats Page</h1>;
 };
 
 export default StatsPage;
