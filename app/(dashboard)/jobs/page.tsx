23a1a5af4b1cbfea598271f0588ace24e8514846
Ardiansyah <eviewnicks@gmail.com>
2025-01-21 11:38:13 +0800

    SetUp search Form

--- app/(dashboard)/jobs/page.tsx
+++ app/(dashboard)/jobs/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { getAllJobsAction } from "@/utils/actions";
+import JobsList from "@/components/JobList";
+import SearchForm from "@/components/SearchForm";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
-const JobsPage = () => {
-  return <h1 className="text-4xl">Jobs Page</h1>;
-};
+async function AllJobsPage() {
+  const queryClient = new QueryClient();
 
-export default JobsPage;
+  await queryClient.prefetchQuery({
+    queryKey: ["jobs", "", "all", 1],
+    queryFn: () => getAllJobsAction({}),
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <SearchForm />
+      <JobsList />
+    </HydrationBoundary>
+  );
+}
+
+export default AllJobsPage;
---
+++ components/JobCard.tsx
@@ -0,0 +1,7 @@
+import { JobType } from "@/utils/types";
+
+function JobCard() {
+  return <div>JobCard</div>;
+}
+
+export default JobCard;
