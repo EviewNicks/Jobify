0e1ddeb2f6e6b422a219b8d5b5414ab25714a09e
Ardiansyah <eviewnicks@gmail.com>
2025-01-21 09:18:01 +0800

    React Query Setup and Create Job Form

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,7 +1,17 @@
 import CreateJobForm from "@/components/CreateJobForm";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 const AddJobsPage = () => {
-  return <CreateJobForm />;
+  const queryClient = new QueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <CreateJobForm />
+    </HydrationBoundary>
+  );
 };
 
 export default AddJobsPage;
